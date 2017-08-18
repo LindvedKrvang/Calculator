20988aeaf8a2da02eaf0babd12e159307706ed16
LindvedKrvang <rklindved@gmail.com>
2017-08-19 00:25:58 +0200

    Implemented FindOperator

--- Calculator/CalculaterManager.cs
+++ Calculator/CalculaterManager.cs
@@ -1,10 +1,19 @@
-﻿namespace Calculator
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
 {
     public class CalculaterManager
     {
         public string FindOperator(string input)
         {
-            throw new System.NotImplementedException();
+            var subStrings = input.Split(' ');
+            var operators = subStrings.First(s =>
+                s.Equals("+") ||
+                s.Equals("-") ||
+                s.Equals("*") ||
+                s.Equals("/"));
+            return operators;
         }
     }
 }
