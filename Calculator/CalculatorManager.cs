1a0fc435b1f9a770bc5f40c2a4b83a7a3162b814
LindvedKrvang <rklindved@gmail.com>
2017-08-18 19:34:17 +0200

    Added unitTest for substract method and added the method

--- Calculator/CalculatorManager.cs
+++ Calculator/CalculatorManager.cs
@@ -4,9 +4,19 @@ namespace Calculator
 {
     public class CalculatorManager
     {
-        public int Add(params int[] numbers)
+        public float Add(params float[] numbers)
         {
             return numbers.Sum();
+        }
+
+        public float Substract(params float[] numbers)
+        {
+            var result = numbers[0];
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                result -= numbers[i];
+            }
+            return result;
         }
     }
 }
--- CalculatorTests/CalculatorManagerTests.cs
+++ CalculatorTests/CalculatorManagerTests.cs
@@ -9,35 +9,73 @@ namespace CalculatorTests
     {
 
         [TestMethod]
-        public void TestAddWithOneInteger()
+        public void TestAddWithOneFLoat()
         {
             var calcManager = new CalculatorManager();
-            var expectedResult = 5;
-            var result = calcManager.Add(5);
+            var expectedResult = 5f;
+            var result = calcManager.Add(5f);
 
             Assert.AreEqual(expectedResult, result);
         }
 
         [TestMethod]
-        public void TestAddWithTwoIntegers()
+        public void TestAddWithTwoFloats()
         {
             var calcManager = new CalculatorManager();
-            var expectedResult = 8;
-            var result = calcManager.Add(4, 4);
+            var expectedResult = 8f;
+            var result = calcManager.Add(4f, 4f);
 
             Assert.AreEqual(expectedResult, result);
         }
 
         [TestMethod]
-        public void TestAddWithThreeIntegers()
+        public void TestAddWithThreeFloats()
         {
             var calcManager = new CalculatorManager();
-            var expectedResult = 10;
-            var result = calcManager.Add(3, 5, 2);
+            var expectedResult = 10f;
+            var result = calcManager.Add(3f, 5f, 2f);
 
             Assert.AreEqual(expectedResult, result);
         }
 
+        [TestMethod]
+        public void TestSubstractWithOneFloat()
+        {
+            var calcManager = new CalculatorManager();
+            var expectedResult = -1;
+            var result = calcManager.Substract(-1);
 
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestSubstractWithTwoFloat()
+        {
+            var calcManager = new CalculatorManager();
+            var expectedResult = -2;
+            var result = calcManager.Substract(5f, 7f);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestSubstractWithTwoFloatsWhereTheLastIsNegative()
+        {
+            var calcManager = new CalculatorManager();
+            var expectedResult = 10f;
+            var result = calcManager.Substract(5f, -5f);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestSubstractWithFiveFloats()
+        {
+            var calcManager = new CalculatorManager();
+            var expectedResult = -2f;
+            var result = calcManager.Substract(2f, -7f, 6f, 4f, 1f);
+
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
