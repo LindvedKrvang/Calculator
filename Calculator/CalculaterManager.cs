17fc394bc8061dc88f96e258c3bfae66321d2122
LindvedKrvang <rklindved@gmail.com>
2017-08-18 23:43:25 +0200

    Added unitTest for FindOperator

---
+++ Calculator/CalculaterManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public class CalculaterManager
+    {
+        public string FindOperator(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- CalculatorTests/CalculatorManagerTests.cs
+++ CalculatorTests/CalculatorManagerTests.cs
@@ -2,11 +2,51 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
 
 namespace CalculatorTests
 {
     [TestClass]
-    class CalculatorManagerTests
+    public class CalculatorManagerTests
     {
+        [TestMethod]
+        public void TestFindOperaterAdd()
+        {
+            var manager = new CalculaterManager();
+            var expectedResult = "+";
+            var result = manager.FindOperator("32 + 40");
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestFindOperatorSubstract()
+        {
+            var manager = new CalculaterManager();
+            var expectedResult = "-";
+            var result = manager.FindOperator("10 - 36");
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestFindOperatorMultiply()
+        {
+            var manager = new CalculaterManager();
+            var expectedResult = "*";
+            var result = manager.FindOperator("3 * 5");
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestFindOperatorDivision()
+        {
+            var manager = new CalculaterManager();
+            var expectedResult = "/";
+            var result = manager.FindOperator("24 / 6");
+
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
